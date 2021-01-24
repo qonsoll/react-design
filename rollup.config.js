64e35cb5e8eb0690a5653bf025903ac30b3085e1
Oleksiy Pastukhov <oleksiy.pastukhov@senseteq.io>
2021-01-24 23:10:51 +0200

    Cleaned rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -4,54 +4,28 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
-import multi from '@rollup/plugin-multi-entry';
 import { terser } from "rollup-plugin-terser";
 
 const packageJson = require("./package.json");
 
 export default {
-  // input: [
-  //   "src/index.ts",
-  //   "src/components/Base/Box/Box.template.tsx",
-  //   "src/components/Grid/Container/Container.template.tsx",
-  //   "src/components/Grid/Row/Row.template.tsx",
-  //   "src/components/Grid/Col/Col.template.tsx"
-  // ],
-  // output: [
-  //   {
-  //     dir: "build",
-  //     format: "cjs",
-  //     sourcemap: true,
-  //     exports: 'named'
-  //   },
-  //   {
-  //     dir: "build",
-  //     format: "esm",
-  //     sourcemap: true,
-  //     exports: 'named'
-  //   }
-  // ],
   input: "src/index.ts",
   output: [
     {
-      // dir: "build",
       file: packageJson.main,
       format: "cjs",
       sourcemap: true
     },
     {
-      // dir: "build",
       file: packageJson.module,
       format: "esm",
       sourcemap: true
     }
   ],
-  // preserveModules: true,
   plugins: [
     babel({
       exclude: "node_modules/**"
     }),
-    // multi(),
     peerDepsExternal(),
     resolve(),
     commonjs(),
