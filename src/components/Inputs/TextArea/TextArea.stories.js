d9356058757dbe14a79f0fd20988501878eef518
Olena <olenashevchuk0@gmail.com>
2021-09-13 17:43:22 +0300

    added argTypes for TextArea

--- src/components/Inputs/TextArea/TextArea.stories.js
+++ src/components/Inputs/TextArea/TextArea.stories.js
@@ -1,28 +1,120 @@
 import React from 'react'
 import TextArea from './TextArea'
+import {
+  spaceArgTypes,
+  colorArgTypes,
+  typographyArgTypes,
+  layoutArgTypes,
+  flexboxArgTypes,
+  backgroundArgTypes,
+  borderArgTypes,
+  positionArgTypes,
+  shadowArgTypes,
+  extraArgTypes
+} from '../../../helpers/arg-types'
 
 export default {
   title: 'Components/Inputs/TextArea',
   component: TextArea,
   argTypes: {
-    'Default AntD props': {
-      description:
-        'Component absolutely maintain default AntD textarea properties (check Ant documentation: https://ant.design/components/input/#Input.TextArea).',
-      control: { disable: true }
+    allowClear: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'If allow to remove input content with clear icon',
+      control: {
+        type: 'boolean'
+      }
     },
+    autoSize: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Height autosize feature, can be set to true | false or an object { minRows: 2, maxRows: 6 }',
+      control: {
+        type: 'object'
+      }
+    },
+    bordered: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Whether has border style',
+      control: {
+        type: 'boolean'
+      }
+    }, 
+    defaultValue: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'The initial input content',
+      control: {
+        type: 'string'
+      }
+    }, 
+    maxLength: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'The max length',
+      control: {
+        type: 'number'
+      }
+    },  
+    showCount: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Whether show text count',
+      control: {
+        type: 'boolean'
+      }
+    },   
+    value: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'The input content value',
+      control: {
+        type: 'string'
+      }
+    },    
+    onPressEnter: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'The callback function that is triggered when Enter key is pressed',
+    },     
+    onResize: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'The callback function that is triggered when resize',
+    },      
     Packages: {
       table: {
-        category: 'Extentions'
+        category: 'Extensions'
       },
       description:
-        'Applyed styled-system packages (check styled-system API documentation: https://styled-system.com/api).',
+        'Applied styled-system packages (check styled-system API documentation: https://styled-system.com/api).',
       control: { disable: true }
     },
+    ...spaceArgTypes,
+    ...colorArgTypes,
+    ...typographyArgTypes,
+    ...layoutArgTypes,
+    ...flexboxArgTypes,
+    ...backgroundArgTypes,
+    ...borderArgTypes,
+    ...positionArgTypes,
+    ...shadowArgTypes,
+    ...extraArgTypes,
     'Extra CSS props': {
       table: {
-        category: 'Extentions'
+        category: 'Extensions'
       },
-      description: 'Additional CSS properties which could be applyed.',
+      description: 'Additional CSS properties which could be applied.',
       control: { disable: true }
     }
   }
