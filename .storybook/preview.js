3b52c32cb99c814a8dd0d37e272435a48601f259
Olena <olenashevchuk0@gmail.com>
2021-09-15 15:57:53 +0300

    fixed aside right, added bg, fixed fontFamily

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,13 +6,17 @@ import '../src/styles/PhoneInput.css'
 export const parameters = {
   layout: 'centered',
   controls: { expanded: true },
+  backgrounds: {
+    default: 'gray',
+    values: [{ name: 'default', value: 'transparent' }, { name: 'gray', value: '#f6f6f666' }]
+  },
   actions: {
     argTypesRegex: '^on[A-Z].*'
   }
 }
 
 export const decorators = [
-  (Story) => (
+  (Story) => (      
     <Story />
   )
 ]
