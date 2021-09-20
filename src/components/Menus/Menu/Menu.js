bedcbcd6f0e59911192055bb19143cb93b2f4873
Olena <olenashevchuk0@gmail.com>
2021-09-20 13:11:30 +0300

    added ArgTypes and PropTypes for Menu, added horizontal and vertical mode

--- src/components/Menus/Menu/Menu.js
+++ src/components/Menus/Menu/Menu.js
@@ -37,30 +37,30 @@ const StyledMenu = styled(AntMenu)(
   )
 )
 
-const Menu = (props) => {
-  return <StyledMenu {...props} />
-}
+const Menu = (props) => <StyledMenu {...props} />
 
 Menu.propTypes = {
-  'Default AntD props': PropTypes.object,
-  Packages: PropTypes.oneOf([
-    'space',
-    'color',
-    'typography',
-    'layout',
-    'flexbox',
-    'background',
-    'border',
-    'position',
-    'shadow'
-  ]),
-  'Extra CSS props': PropTypes.oneOf([
-    'whiteSpace',
-    'cursor',
-    'wordBreak',
-    'zoom',
-    'transform'
-  ])
+  defaultOpenKeys: PropTypes.arrayOf(PropTypes.string),
+  defaultSelectedKeys: PropTypes.arrayOf(PropTypes.string),
+  expandIcon: PropTypes.node,
+  forceSubMenuRender: PropTypes.bool,
+  inlineCollapsed: PropTypes.bool,
+  inlineIndent: PropTypes.number,
+  mode: PropTypes.oneOf(['vertical', 'horizontal', 'inline']),
+  multiple: PropTypes.bool,
+  openKeys: PropTypes.arrayOf(PropTypes.string),
+  overflowedIndicator: PropTypes.node,
+  selectable: PropTypes.bool,
+  selectedKeys: PropTypes.arrayOf(PropTypes.string),
+  style: PropTypes.object,
+  subMenuCloseDelay: PropTypes.number,
+  subMenuOpenDelay: PropTypes.number,
+  theme: PropTypes.oneOf(['dark', 'light']),
+  triggerSubMenuAction: PropTypes.oneOf(['hover', 'click']),
+  onClick: PropTypes.func,
+  onDeselect: PropTypes.func,
+  onOpenChange: PropTypes.func,
+  onSelect: PropTypes.func
 }
 
 export default Menu
--- src/components/Menus/Menu/Menu.stories.js
+++ src/components/Menus/Menu/Menu.stories.js
@@ -1,31 +1,236 @@
 import React from 'react'
 import Menu from './Menu'
+import MenuItem from '../MenuItem'
+import Group from '../Group'
+import Submenu from '../Submenu'
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined
+} from '@ant-design/icons'
+import {
+  spaceArgTypes,
+  colorArgTypes,
+  typographyArgTypes,
+  layoutArgTypes,
+  flexboxArgTypes,
+  backgroundArgTypes,
+  borderArgTypes,
+  positionArgTypes,
+  shadowArgTypes
+} from '../../../helpers/arg-types'
+import CSSPropValues from '../../../helpers/css-prop-values'
 
 export default {
   title: 'Components/Menus/Menu',
   component: Menu,
+  parameters: {
+    layout: 'fullscreen'
+  },
   argTypes: {
-    'Default AntD props': {
-      description:
-        'Component absolutely maintain default AntD menu properties (check Ant documentation: https://ant.design/components/menu/#Menu).',
-      control: { disable: true }
-    },
-    Packages: {
+    defaultOpenKeys: {
       table: {
-        category: 'Extentions'
+        category: 'Default AntD props'
       },
-      description:
-        'Applyed styled-system packages (check styled-system API documentation: https://styled-system.com/api).',
-      control: { disable: true }
+      description: 'Array with the keys of default opened sub menus	'
     },
-    'Extra CSS props': {
+    defaultSelectedKeys: {
       table: {
-        category: 'Extentions'
+        category: 'Default AntD props'
       },
-      description: 'Additional CSS properties which could be applyed.',
-      control: { disable: true }
+      description: 'Array with the keys of default selected menu items	'
+    },
+    expandIcon: {
+      table: {
+        category: 'Default AntD props'
+      },
+      control: { disable: true },
+      description: 'Custom expand icon of submenu	'
+    },
+    forceSubMenuRender: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Render submenu into DOM before it becomes visible	'
+    },
+    inlineCollapsed: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Specifies the collapsed status when menu is inline mode	'
+    },
+    inlineIndent: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Indent (in pixels) of inline menu items on each level	'
+    },
+    mode: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Type of menu	'
+    },
+    multiple: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Allows selection of multiple items	'
+    },
+    openKeys: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Array with the keys of currently opened sub-menus	'
+    },
+    overflowedIndicator: {
+      table: {
+        category: 'Default AntD props'
+      },
+      control: { disable: true },
+      description: 'Customized icon when menu is collapsed	'
+    },
+    selectable: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Allows selecting menu items	'
+    },
+    selectedKeys: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Array with the keys of currently selected menu items	'
+    },
+    style: {
+      table: {
+        category: 'Default AntD props'
+      },
+      control: { disable: true },
+      description: 'Style of the root node	'
+    },
+    subMenuCloseDelay: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Delay time to hide submenu when mouse leaves (in seconds)	'
+    },
+    subMenuOpenDelay: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Delay time to show submenu when mouse enters, (in seconds)	'
+    },
+    theme: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Color theme of the menu	'
+    },
+    triggerSubMenuAction: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Which action can trigger submenu open/close	'
+    },
+    onClick: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Called when a menu item is clicked	'
+    },
+    onDeselect: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Called when a menu item is deselected (multiple mode only)	'
+    },
+    onOpenChange: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Called when sub-menus are opened or closed	'
+    },
+    onSelect: {
+      table: {
+        category: 'Default AntD props'
+      },
+      description: 'Called when a menu item is selected	'
+    },
+    ...spaceArgTypes,
+    ...colorArgTypes,
+    ...typographyArgTypes,
+    ...layoutArgTypes,
+    ...flexboxArgTypes,
+    ...backgroundArgTypes,
+    ...borderArgTypes,
+    ...positionArgTypes,
+    ...shadowArgTypes,
+    whiteSpace: {
+      table: {
+        category: 'Extra CSS props'
+      },
+      control: CSSPropValues.whiteSpace
+    },
+    cursor: {
+      table: {
+        category: 'Extra CSS props'
+      },
+      control: CSSPropValues.cursor
+    },
+    wordBreak: {
+      table: {
+        category: 'Extra CSS props'
+      },
+      control: CSSPropValues.wordBreak
+    },
+    zoom: {
+      table: {
+        category: 'Extra CSS props'
+      },
+      control: CSSPropValues.zoom
+    },
+    transform: {
+      table: {
+        category: 'Extra CSS props'
+      },
+      control: CSSPropValues.transform
     }
-  }
+  },
+  args: { defaultSelectedKeys: 'mail' }
 }
 
-export const Template = (args) => <Menu {...args} />
+const Template = (args) => (
+  <Menu {...args}>
+    <MenuItem key="mail" icon={<MailOutlined />}>
+      Navigation One
+    </MenuItem>
+    <MenuItem key="app" disabled icon={<AppstoreOutlined />}>
+      Navigation Two
+    </MenuItem>
+    <Submenu
+      key="SubMenu"
+      icon={<SettingOutlined />}
+      title="Navigation Three - Submenu">
+      <Group title="Item 1">
+        <MenuItem key="setting:1">Option 1</MenuItem>
+        <MenuItem key="setting:2">Option 2</MenuItem>
+      </Group>
+      <Group title="Item 2">
+        <MenuItem key="setting:3">Option 3</MenuItem>
+        <MenuItem key="setting:4">Option 4</MenuItem>
+      </Group>
+    </Submenu>
+    <MenuItem key="alipay">
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    </MenuItem>
+  </Menu>
+)
+
+export const horizontalMenu = Template.bind({})
+horizontalMenu.args = { mode: 'horizontal' }
+
+export const verticalMenu = Template.bind({})
+verticalMenu.args = { mode: 'vertical', width: '190px' }
